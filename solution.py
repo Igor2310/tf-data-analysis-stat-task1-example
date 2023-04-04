0edc0b61ab5c34af897265b69038e8d1ee9c6e40
Igor2310 <80477307+Igor2310@users.noreply.github.com>
2023-04-04 10:18:44 +0300

    first attempt 04.04.2023

--- solution.py
+++ solution.py
@@ -2,10 +2,14 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1152225195 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    x_mean = (3-np.exp(1))/20 # ср. ускорение
+    x=x/10
+    difference = x-x_mean
+    squared_difference = np.square(difference)
+    return squared_difference.mean() # Ваш ответ
