451805d241718c9107adbeab8dc01723579eaccd
Igor2310 <80477307+Igor2310@users.noreply.github.com>
2023-04-04 13:47:38 +0300

    second attempt 04.04.2023

--- solution.py
+++ solution.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import numpy as np
 
 
@@ -8,8 +7,4 @@ def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    x_mean = (3-np.exp(1))/20 # ср. ускорение
-    x=x/10
-    difference = x-x_mean
-    squared_difference = np.square(difference)
-    return squared_difference.mean() # Ваш ответ
+    return x.mean()/10 # Ваш ответ
