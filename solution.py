f73fcc0cc0ccbe52bdc89661d9b476bcfcbc1782
Igor2310 <80477307+Igor2310@users.noreply.github.com>
2023-04-08 01:49:34 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,9 @@
+import pandas as pd
 import numpy as np
 
 
-chat_id = 1152225195 # Ваш chat ID, не меняйте название переменной
+chat_id = 728846853 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    a=x.mean()/(3-np.exp(1))
-    return a/10 # Ваш ответ
+    a = (x.mean()+9)/10
+    return a # Ваш ответ
