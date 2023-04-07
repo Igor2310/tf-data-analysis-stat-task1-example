a0812bd3597e22d60cc1c55aa12c890bd0f621ac
Igor2310 <80477307+Igor2310@users.noreply.github.com>
2023-04-08 01:50:09 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 728846853 # Ваш chat ID, не меняйте название переменной
+chat_id = 1152225195 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
     a = (x.mean()+9)/10
