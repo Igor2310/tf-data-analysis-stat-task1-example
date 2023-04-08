3a5263e21201552658ffae19c29343939e039743
Igor2310 <80477307+Igor2310@users.noreply.github.com>
2023-04-08 05:41:20 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,5 +5,5 @@ import numpy as np
 chat_id = 1152225195 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    a = (x.mean()+9)/10
+    a=x.mean()+(3.5*(3-np.exp(1)))
     return a # Ваш ответ
